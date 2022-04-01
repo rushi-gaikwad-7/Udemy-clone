16133916fc3f7d86a6975767c124a88985540729
AmanJaiswal0612 <aman.jaiswal0612@gmail.com>
2022-04-01 15:34:25 +0530

    added cart

--- PaymentGateway/payment.js
+++ PaymentGateway/payment.js
@@ -2,13 +2,17 @@ let all_total_price = document.getElementById("all_total_price");
 let discounted_price = document.getElementById("discounted_price");
 let total = document.getElementById("total")
 
-let total_discount_price = JSON.parse(localStorage.getItem("total_discount_price"));
-console.log(total_discount_price);
+// let total_discount_price = JSON.parse(localStorage.getItem("total_discount_price"));
+// console.log(total_discount_price);
 
-all_total_price.innerHTML = `&#8377;${total_discount_price[0].total_price}`;
-discounted_price.innerHTML = `-&#8377;${total_discount_price[0].discount_price}`;
-total.innerHTML = `&#8377;${Number(total_discount_price[0].total_price) - Number(total_discount_price[0].discount_price)}`;
+// all_total_price.innerHTML = `&#8377;${total_discount_price[0].total_price}`;
+// discounted_price.innerHTML = `-&#8377;${total_discount_price[0].discount_price}`;
+// total.innerHTML = `&#8377;${Number(total_discount_price[0].total_price) - Number(total_discount_price[0].discount_price)}`;
 
+let totalcartprice= localStorage.getItem("totalcartprice");
+all_total_price.innerText= Math.floor(Number((totalcartprice*100)/80));
+discounted_price.innerText= totalcartprice;
+total.innerText=totalcartprice;
 function shoppingComplete() {
   let array = [
     "Payment Successfull",
