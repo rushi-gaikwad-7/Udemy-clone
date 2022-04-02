9dee7b291b528ce5bf77c74f43651f6c4e5a0d5f
rushig777 <gaikwadrushi777@gmail.com>
2022-04-02 09:29:52 +0530

    payment is okay now

--- PaymentGateway/payment.js
+++ PaymentGateway/payment.js
@@ -2,12 +2,6 @@ let all_total_price = document.getElementById("all_total_price");
 let discounted_price = document.getElementById("discounted_price");
 let total = document.getElementById("total")
 
-// let total_discount_price = JSON.parse(localStorage.getItem("total_discount_price"));
-// console.log(total_discount_price);
-
-// all_total_price.innerHTML = `&#8377;${total_discount_price[0].total_price}`;
-// discounted_price.innerHTML = `-&#8377;${total_discount_price[0].discount_price}`;
-// total.innerHTML = `&#8377;${Number(total_discount_price[0].total_price) - Number(total_discount_price[0].discount_price)}`;
 
 let totalcartprice= localStorage.getItem("totalcartprice");
 all_total_price.innerText= Math.floor(Number((totalcartprice*100)/80));
@@ -32,45 +26,31 @@ function shoppingComplete() {
     h1.textContent = array[index++];
   }, 8000);
 }
-function checkSpaces(data) {
-  var count = 0;
-  for (let i = 0; i < data.length; i++) {
-    if (data[i] == " ") {
-      count++;
-    }
-  }
-  return count;
-}
-function modifyFullname(data) {
-  if (checkSpaces(data) == 1) {
 
-    var res = data[0].toUpperCase();
-    for (let i = 1; i < data.length; i++) {
-      if (data[i] == " ") {
-        res += data[i + 1].toUpperCase()
-        break;
-      }
-    }
-    return res;
-  } else {
-    res = data[0].toUpperCase();
-    for (let j = data.length - 1; j >= 1; j--) {
-      if (data[j] == " ") {
-        res += data[j + 1].toUpperCase()
-        break;
-      }
-    }
-    return res;
-  }
-}
-let loginDetails = JSON.parse(localStorage.getItem('loginDetails'));
-let udemyUsers = JSON.parse(localStorage.getItem('udemyUsers'));
-let btnUser = document.getElementById('btnUser');
-for (let i = 0; i < loginDetails.length; i++) {
-  for (let j = 0; j < udemyUsers.length; j++) {
-    if (loginDetails[i].loginEmail == udemyUsers[j].signupEmail) {
-      btnUser.innerText = `${modifyFullname(udemyUsers[j].signupFullname)}`
-    }
-  }
 
+
+let  sortbox= document.querySelector("#sortitem")    
+let data= JSON.parse(localStorage.getItem("cartproducts"))||[];
+displayFiltersearch(data);
+
+function displayFiltersearch(data){
+sortbox.innerHTML="";
+data.map((el,index)=>{
+ let box= document.createElement('div');
+ box.id="box";
+ let image= document.createElement('img');
+ image.src=el.image;
+ let title= document.createElement("h4");
+ title.innerText=el.title;
+let pricebox= document.createElement("div");
+pricebox.setAttribute("id","pricebox")
+pricebox.style.display="block"
+let price= document.createElement("h4");
+price.innerText="Rs"+el.price;
+
+
+pricebox.append(price)
+box.append(image,title,pricebox);
+sortbox.append(box);
+})
 }
